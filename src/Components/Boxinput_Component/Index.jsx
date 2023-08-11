0d3b315f25a53c2954d2f864259bedac20f239aa
Y Kim Tra <tra.y@napaglobal.com>
2023-08-11 09:59:02 +0700

    todo

--- src/Components/Boxinput_Component/Index.jsx
+++ src/Components/Boxinput_Component/Index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./style.css";
 
+
 const index = ({ activeTab }) => {
   return (
     <div className="main">
@@ -17,7 +18,7 @@ const index = ({ activeTab }) => {
           </div>
           <div className="result">
             <div className="result-text">
-              <div className="checkbox"></div>
+              <input type="checkbox" className="checkbox" />
               <div className="text">Do coding challenges</div>
             </div>
           </div>
@@ -38,7 +39,7 @@ const index = ({ activeTab }) => {
           </div>
           <div className="result">
             <div className="result-text">
-              <div className="checkbox"></div>
+              <input type="checkbox" className="checkbox" />
               <div className="text">Do coding challenges</div>
             </div>
           </div>
@@ -47,9 +48,9 @@ const index = ({ activeTab }) => {
 
       {activeTab === "completed" && (
         <>
-          {/* <div className="result">
+          <div className="result">
             <div className="result-text">
-              <div className="checkbox"></div>
+              <input type="checkbox" className="checkbox" />
               <div className="text">Do coding challenges</div>
             </div>
             <div className="icon-delete">
@@ -58,7 +59,7 @@ const index = ({ activeTab }) => {
                 <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
               </svg>
             </div>
-          </div> */}
+          </div>
           <div className="delete">
               <div className="icon-delete-btn">
                 <svg 
