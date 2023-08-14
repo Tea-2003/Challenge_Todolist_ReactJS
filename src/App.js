27c6990ef46dba13ad8c6e690d92090281eb0af3
Y Kim Tra <tra.y@napaglobal.com>
2023-08-14 10:33:43 +0700

    fix deploy vercel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import React, { useState } from 'react';
-import Boxinput_Component from "./Components/Boxinput_Component";
-import NavActive_Component from "./Components/NavActive_Component";
-import NavAll_Component from "./Components/NavAll_Component";
-import NavComplete_Component from "./Components/NavComplete_Component";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React, { useState } from "react";
+import Boxinput_Component from "./Components/Boxinput_Component/index";
+import NavActive_Component from "./Components/NavActive_Component/index";
+import NavAll_Component from "./Components/NavAll_Component/index";
+import NavComplete_Component from "./Components/NavComplete_Component/index";
 
 const App = () => {
   const [activeTab, setActiveTab] = useState("all");
@@ -16,12 +16,24 @@ const App = () => {
           <div className="todo">#todo</div>
 
           <div className="navigation">
-            <NavAll_Component setActiveTab={setActiveTab} activeTab={activeTab} />
-            <NavActive_Component setActiveTab={setActiveTab} activeTab={activeTab}/>
-            <NavComplete_Component setActiveTab={setActiveTab} activeTab={activeTab}/>
+            <NavAll_Component
+              setActiveTab={setActiveTab}
+              activeTab={activeTab}
+            />
+            <NavActive_Component
+              setActiveTab={setActiveTab}
+              activeTab={activeTab}
+            />
+            <NavComplete_Component
+              setActiveTab={setActiveTab}
+              activeTab={activeTab}
+            />
           </div>
           <Routes>
-          <Route path="/" element={<Boxinput_Component activeTab={activeTab} />} />
+            <Route
+              path="/"
+              element={<Boxinput_Component activeTab={activeTab} />}
+            />
           </Routes>
         </div>
         <Boxinput_Component />
